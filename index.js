1438b261573e2a6a8ae72f5ae22c6456fc385faf
abhishek nath <abhishek78nath@gmail.com>
2023-01-11 00:11:16 +0530

    second request

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-const port = 5000;
+const port = 5000 || process.env.PORT;
 const app = express();
 
 // Database connection
@@ -15,9 +15,11 @@ app.use(express.json());
 app.use('/', require('./router/visitorCount'))
 app.use('/auth', require('./router/auth'));
 app.get("/", (req,res) =>{
-    res.send('<h2>Welcome to Microtask 2')
+    res.send('<h2>Welcome to Microtask 2</h2><br><p>Access the visitor counter with endpoint "/visCount", if it gives bad request refresh the page again<br>Access the authentication in api tester like thunderclient or postman with endpoints "/signup", "/login", "/getuser"')
 })
 
 app.listen(port, (req,res) =>{
     console.log(`Server has started at http://127.0.0.1:${port}`);
 })
+
+module.exports = app;
